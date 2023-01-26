812909e12c437344d943c3538081cf04f27ee1f6
join93 <109458670+join93@users.noreply.github.com>
2023-01-26 19:22:12 +0200

    Add files via upload

---
+++ softcams/astra-sm/astra-sm.sh
@@ -0,0 +1,45 @@
+cd /tmp
+set -e
+wget "https://raw.githubusercontent.com/join93/feed/main/softcams/astra-sm/astra-sm.tar.gz"
+tar -xzf enigma2-astra.tar.gz -C /
+set +e
+rm -f enigma2-astra.tar.gz
+wait
+opkg install https://raw.githubusercontent.com/join93/feed/main/softcams/astra-sm/astra-sm.ipk
+wait
+sleep 2;
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
