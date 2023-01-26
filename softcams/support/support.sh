7fd43ec164b18030b8b70fd5931a84c377a23d43
join93 <109458670+join93@users.noreply.github.com>
2023-01-26 20:17:47 +0200

    Add files via upload

---
+++ softcams/support/support.sh
@@ -0,0 +1,40 @@
+echo " Installing Softcam Support Please Wait... " 
+
+opkg install --force-overwrite https://raw.githubusercontent.com/join93/feed/main/support/support.ipk
+wait
+sleep 2;
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
