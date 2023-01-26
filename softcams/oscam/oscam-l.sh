78fe6618ecf430bb9249569a79aca05015c18da0
join93 <109458670+join93@users.noreply.github.com>
2023-01-26 19:57:06 +0200

    Add files via upload

---
+++ softcams/oscam/oscam-l.sh
@@ -0,0 +1,42 @@
+echo " Installing Oscam Please Wait... " 
+
+opkg install --force-overwrite https://raw.githubusercontent.com/join93/feed/main/softcams/oscam/oscam-l.ipk
+ 
+wait 
+sleep 2; 
+exit 0 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
