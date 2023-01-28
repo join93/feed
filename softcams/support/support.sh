a37e9fcaa6d1ee38787263c508fa8885d308ddd6
join93 <109458670+join93@users.noreply.github.com>
2023-01-28 15:02:24 +0200

    Update support.sh

--- softcams/support/support.sh
+++ softcams/support/support.sh
@@ -1,4 +1,4 @@
-opkg install https://raw.githubusercontent.com/join93/feed/main/support/support.ipk
+opkg install https://raw.githubusercontent.com/join93/feed/main/softcams/support/support.ipk
 wait
 sleep 2;
 exit 0
