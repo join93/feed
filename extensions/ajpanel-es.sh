3cbfb2d5284b799402a7fac68b63f1e4f4f8b3d7
join93 <109458670+join93@users.noreply.github.com>
2023-01-28 13:55:36 +0200

    Add files via upload

---
+++ extensions/ajpanel-es.sh
@@ -0,0 +1,17 @@
+##wget https://raw.githubusercontent.com/join93/feed/main/extensions/ajpanel-dev.sh -O - | /bin/sh##
+
+version=8.4.0
+
+cd /tmp
+set -e
+
+wget --no-check-certificate https://raw.githubusercontent.com/join93/feed/main/extensions/ajpanel-"$version".tar.gz
+tar -xzf ajpanel-"$version".tar.gz -C /
+
+rm -rf *ajpanel-"$version".tar.gz* >/dev/null 2>&1
+
+echo
+set +e
+cd ..
+echo "#                 ajpanel-"$version" installed successfully            #"
+exit 
