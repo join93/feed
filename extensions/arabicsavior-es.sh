554139169c5d360816550493b9c8507b81f4df49
join93 <109458670+join93@users.noreply.github.com>
2023-01-28 20:07:19 +0200

    Add files via upload

---
+++ extensions/arabicsavior-es.sh
@@ -0,0 +1,71 @@
+##wget https://raw.githubusercontent.com/join93/feed/main/extensions/arabicsavior-es.sh -O - | /bin/sh##
+
+version=1.9
+
+
+cd /tmp
+rm -f *arabicsavior*
+# Download new version
+wget "https://raw.githubusercontent.com/join93/feed/main/extensions/arabicsavior-"$version".tar.gz"
+
+# remove old version
+rm -r /usr/lib/enigma2/python/Plugins/Extensions/ArabicSavior > /dev/null 2>&1
+
+# Install new version
+tar -xzf arabicsavior-"$version".tar.gz -C /
+set +e
+rm -f *arabicsavior*
+cd ..
+
+sync
+echo ""#                 arabicsavior-"$version" installed successfully            #""
+
+sleep 3
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
