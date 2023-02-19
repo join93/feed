b8a63c91e8d589fd60180787fcdb99f993fc3151
join93 <109458670+join93@users.noreply.github.com>
2023-02-19 10:06:45 +0200

    Add files via upload

---
+++ extensions/ajpanel-config.sh
@@ -0,0 +1,37 @@
+#checking and installing ajpanel 
+
+if [ ! -d "/usr/lib/enigma2/python/Plugins/Extensions/AJPan" ]; then
+wget https://raw.githubusercontent.com/join93/feed/main/extensions/ajpanel-es.sh -O - | /bin/sh
+fi
+#download ajpanel settings and folder
+
+
+echo ""
+cd /tmp
+set -e
+wget -O ajpanel-folder.tar.gz "https://raw.githubusercontent.com/join93/feed/main/extensions/ajpanel-folder.tar.gz"
+
+wget -O ajpanel-settings.tar.gz "https://raw.githubusercontent.com/join93/feed/main/extensions/ajpanel-settings.tar.gz"
+
+
+tar -xzf ajpanel-folder.tar.gz -C /
+tar -xzf ajpanel-settings.tar.gz -C /
+set +e
+rm -f ajpanel-folder.tar.gz
+rm -f ajpanel-settings.tar.gz
+echo''
+if [ -d "/ajpanel" ]; then wget -O /ajpanel/LinuxCommands-openpli.lst "https://raw.githubusercontent.com/join93/feed/main/settings/LinuxCommands-openpli.lst"
+fi
+
+init 4; sleep 5
+
+BACKUP='/tmp/settingsajpanel'
+SETTINGS='/etc/enigma2/settings'
+
+cat $BACKUP >> $SETTINGS
+rm -f $BACKUP
+   
+init 3
+echo " "
+exit
+
