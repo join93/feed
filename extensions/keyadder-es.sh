53f7242b676292716174f897c6c9b8a253eda8d4
join93 <109458670+join93@users.noreply.github.com>
2023-02-20 09:24:35 +0200

    Add files via upload

---
+++ extensions/keyadder-es.sh
@@ -0,0 +1,17 @@
+##wget https://raw.githubusercontent.com/join93/feed/main/extensions/keyadder-es.sh -O - | /bin/sh##
+
+version=6.4
+
+cd /tmp
+set -e
+
+wget --no-check-certificate https://raw.githubusercontent.com/join93/feed/main/extensions/keyadder-"$version".tar.gz
+tar -xzf keyadder-"$version".tar.gz -C /
+
+rm -rf *ajpanel-"$version".tar.gz* >/dev/null 2>&1
+
+echo
+set +e
+cd ..
+echo "#                 keyadder-"$version" installed successfully            #"
+exit 
